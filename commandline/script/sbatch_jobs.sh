37372e4ad525267e599ab47e4c502c7d24e3eab3
ãariken26@outlook.jp <you@example.com>
2026-01-14 03:14:58 +0000

    test

--- commandline/script/sbatch_jobs.sh
+++ commandline/script/sbatch_jobs.sh
@@ -13,11 +13,11 @@ fi
 CONFIG=$1
 
 # ====== ディレクトリ設定 ======
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-CMD_DIR=$(cd -- "$SCRIPT_DIR/.." &> /dev/null && pwd)
+CMD_DIR=/home/arimoto/opt/scensim_env/scenargie_simulator/2.2/scenarios_linux/srm_interference/commandline
+
 cd "$CMD_DIR" || exit
 
 # ====== 実行 ======
-echo "対象ディレクトリ: $SCRIPT_DIR"
+echo "対象ディレクトリ: $CMD_DIR"
 echo "Running simulation with config: $CONFIG"
 ./sim "$CONFIG"
