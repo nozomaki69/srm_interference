c29359f4e5186d1d8f657261f408f1db25581cd4
ãariken26@outlook.jp <you@example.com>
2026-01-14 02:06:39 +0000

    test

---
+++ commandline/script/sbatch_jobs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -J sim_test
+#SBATCH -o logs/%x_%j.out
+#SBATCH -e logs/%x_%j.err
+#SBATCH -t 01:00:00
+#SBATCH -c 1
+#SBATCH --mem=2G
+
+# ====== 引数チェック ======
+if [ $# -ne 1 ]; then
+  echo "Usage: sbatch run_one_sim.slurm <config file>"
+  exit 1
+fi
+
+CONFIG=$1
+
+# ====== ディレクトリ設定 ======
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+CMD_DIR=$(cd -- "$SCRIPT_DIR/.." &> /dev/null && pwd)
+cd "$CMD_DIR" || exit
+
+# ====== 実行 ======
+echo "Running simulation with config: $CONFIG"
+./sim "$CONFIG"
