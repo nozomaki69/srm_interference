ce6dccfa1d5946b81b74720b07fd06fca88ea653
ãariken26@outlook.jp <you@example.com>
2026-01-14 03:56:45 +0000

    test

--- commandline/script/sbatch_jobs.sh
+++ commandline/script/sbatch_jobs.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
-#SBATCH -o /home/arimoto/opt/scensim_env/scenargie_simulator/2.2/scenarios_linux/srm_interference/commandline/logs/%x_%j.out
-#SBATCH -e /home/arimoto/opt/scensim_env/scenargie_simulator/2.2/scenarios_linux/srm_interference/commandline/logs/%x_%j.err
 
 CMD_DIR=/home/arimoto/opt/scensim_env/scenargie_simulator/2.2/scenarios_linux/srm_interference/commandline
 SCRIPT_DIR="$CMD_DIR/script"
 
 cd "$CMD_DIR" || exit 1
 
-count=0
-
 for config in *.config; do
-  # *.config が1つも無いときの対策
   [ -e "$config" ] || continue
-
   sbatch "$SCRIPT_DIR/run_one_sim.slurm" "$config"
   echo "submitted: $config"
-
-  count=$((count + 1))
 done
-
-echo "----------------------------------------"
-echo "Total submitted jobs: $count"
 
 echo "全てのジョブを投入しました"
