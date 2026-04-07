1015f6a5d0e9546b3f00084b7f38600e8cc28dfe
ãariken26@outlook.jp <you@example.com>
2026-04-07 10:44:37 +0000

    a

--- commandline/script/analyze_trace.sh
+++ commandline/script/analyze_trace.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #SBATCH -J stats_job
-#SBATCH -c 32             # 【重要】1タスクあたりのコア数（30コア使いたい場合）
+#SBATCH -c 32      
 
-python3 ./script/interference_2pan_plot_results.py
+python3 ./script/interference_2pan_plot_result.py
