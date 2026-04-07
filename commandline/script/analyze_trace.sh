c5de68c829bca25156b438a637bade7de40018b6
arimoto <arimoto.kengo.22@shizuoka.ac.jp>
2026-04-07 19:29:49 +0900

    a

--- commandline/script/analyze_trace.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#SBATCH -J stats_job
-#SBATCH -c 32             # 【重要】1タスクあたりのコア数（30コア使いたい場合）
-
-python3 ./script/interference_2pan_plot_results.py
---
+++ script/analyze_trace.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#SBATCH -J stats_job
+#SBATCH -c 32             # 【重要】1タスクあたりのコア数（30コア使いたい場合）
+
+python3 ./script/test_plot.py
