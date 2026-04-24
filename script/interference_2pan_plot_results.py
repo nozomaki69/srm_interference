b9a22c7939c58470d92532d8c7432cb0bb63e611
ãariken26@outlook.jp <you@example.com>
2026-04-24 23:12:17 +0000

    a

--- script/interference_2pan_plot_results.py
+++ script/interference_2pan_plot_results.py
@@ -687,17 +687,27 @@ def eplot_roc_curves(results ,dist_mesure, filename, pan2_val=0.8):
     for off_load in off_load_list:
         key_interf = f"interf_pan1_{off_load}_pan2_{pan2_val:.1f}"
         
+        data_p = []
+        data_n = []
+        target_indices = {0, 4, 7, 11}    # 対象のデバイス番号
+        for i, (rssi, dist)  in enumerate(zip(results["pan1_device_rssi"][key_interf], results["distance_pan1"][key_interf])):
+            # 12で割った余りが 0, 4, 7, 11 なら抽出
+            if i % 12 in target_indices:
+                if dist <= dist_mesure:
+                    data_p.append(rssi)
+                else:
+                    data_n.append(rssi)
         # データの取得（辞書から配列を取り出す）
-        data_p = [
-        diff for diff, dist in zip(results["pan1_diff"][key_interf], results["distance_pan1"][key_interf])
-        if dist < dist_mesure
-        ]
+        # data_p = [
+        # diff for diff, dist in zip(results["pan1_diff"][key_interf], results["distance_pan1"][key_interf])
+        # if dist < dist_mesure
+        # ]
 
-        # 干渉なしデータの抽出
-        data_n = [
-        diff for diff, dist in zip(results["pan1_diff"][key_interf], results["distance_pan1"][key_interf])
-        if dist > dist_mesure
-        ]
+        # # 干渉なしデータの抽出
+        # data_n = [
+        # diff for diff, dist in zip(results["pan1_diff"][key_interf], results["distance_pan1"][key_interf])
+        # if dist > dist_mesure
+        # ]
         
         # ラベルとスコアの結合
         y_true = np.concatenate([np.ones(len(data_p)), np.zeros(len(data_n))])
@@ -743,17 +753,27 @@ def plot_roc_diff_curves(results, off_load, dist_mesure, filename, pan2_val=0.8)
     
     key_interf = f"interf_pan1_{off_load}_pan2_{pan2_val:.1f}"
     
+    data_p = []
+    data_n = []
+    target_indices = {0, 4, 7, 11}    # 対象のデバイス番号
+    for i, (rssi, dist)  in enumerate(zip(results["pan1_device_rssi"][key_interf], results["distance_pan1"][key_interf])):
+        # 12で割った余りが 0, 4, 7, 11 なら抽出
+        if i % 12 in target_indices:
+            if dist <= dist_mesure:
+                data_p.append(rssi)
+            else:
+                data_n.append(rssi)
     # データの取得（辞書から配列を取り出す）
-    data_p = [
-        diff for diff, dist in zip(results["pan1_diff"][key_interf], results["distance_pan1"][key_interf])
-        if dist <= dist_mesure
-    ]
+    # data_p = [
+    #     diff for diff, dist in zip(results["pan1_diff"][key_interf], results["distance_pan1"][key_interf])
+    #     if dist <= dist_mesure
+    # ]
 
-    # 干渉なしデータの抽出
-    data_n = [
-        diff for diff, dist in zip(results["pan1_diff"][key_interf], results["distance_pan1"][key_interf])
-        if dist > dist_mesure
-    ]
+    # # 干渉なしデータの抽出
+    # data_n = [
+    #     diff for diff, dist in zip(results["pan1_diff"][key_interf], results["distance_pan1"][key_interf])
+    #     if dist > dist_mesure
+    # ]
     
     # ラベルとスコアの結合
     y_true = np.concatenate([np.ones(len(data_p)), np.zeros(len(data_n))])
